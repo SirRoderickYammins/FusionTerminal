f71df1bb7630345896aa450a96df03dd5b92ec74
Bakr <bakrbali@gmail.com>
2022-07-07 10:20:47 -0400

    Main

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import inquirer from "inquirer";
+
+const questions = [
+    {
+        type: "editor",
+        name: "bio",
+        message: "Please write a short bio of at least 3 lines",
+        validate(text: string) {
+            if (text.split("\n").length < 3) {
+                return "Must be at least 3 lines.";
+            }
+            return true;
+        },
+    },
+];
+
+inquirer.prompt(questions).then((answers) => {
+    console.log(JSON.stringify(answers, null, " "));
+});
