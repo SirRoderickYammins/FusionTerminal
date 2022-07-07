97aaca89fd97f887ebad30d8e9d299c98222e254
Leke Dobruna <leke.dobruna@gmail.com>
2022-07-07 22:51:58 +0200

    Updated structure

--- src/matrix.ts
+++ src/matrix.ts
@@ -1,15 +1,22 @@
 import axios from "axios";
-import 'dotenv/config';
+import { accessToken } from "./api";
 
-
-
-axios
-    .post("https://matrix.fusionacademy.com/Account/Login", {
-        UserName: process.env.USER_NAME,
-        Password: process.env.PASSWORD,
-    })
-    .then((response) => {
-        console.log("Login Successful!");
-    });
-
-
+export const getShit = () => {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(
+        "https://matrix-api.fusionacademy.com/api/TodaysSchedule?=&campusId=51&startTime=2022-07-07T04:00:00&endTime=2022-07-08T03:00:00",
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      )
+      .then((res) => {
+        resolve(res.data);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
