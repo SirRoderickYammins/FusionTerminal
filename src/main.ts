a9d2f0107a5352176c5707ff284c99c698afe324
Bakr Ali <bakrbali@gmail.com>
2022-07-08 12:19:48 -0400

    Cleaned up

--- src/main.ts
+++ src/main.ts
@@ -2,48 +2,8 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import prompts from "prompts";
 import { login } from "./api";
-import { getTodaysSchedule, setSessionStatus } from "./matrix";
-import { initialActionSelect } from "./prompts/initialActionSelect";
-import { getTodaySelection } from "./prompts/getTodaySelect";
-import { getSessionActions } from "./prompts/getSessionActions";
-import { Session } from "./types/sessionTypes";
+import {PAGES} from "./pages";
 
-const PAGES = {
-  homepage: async () => {
-    console.clear();
-    const response = await initialActionSelect();
-    if (response.initialActionSelect === "lookSchedule") {
-      await PAGES.todaySchedule();
-    }
-  },
-  todaySchedule: async () => {
-    console.clear();
-    console.log("Loading schedule...");
-    const schedule = await getTodaysSchedule();
-    console.clear();
-    const response = await getTodaySelection(schedule.sessions);
-
-    if (response.sessionSelect === -1) {
-      await PAGES.homepage();
-    } else {
-      const session = schedule.sessions[response.sessionSelect];
-      await PAGES.sessionStuff(session);
-    }
-  },
-  sessionStuff: async (session: Session) => {
-    console.clear();
-    const response = await getSessionActions(session);
-    if (response.sessionActions === "attended") {
-      await setSessionStatus(session, true);
-      await PAGES.todaySchedule();
-    } else if (response.sessionActions === "absent") {
-      await setSessionStatus(session, false);
-      await PAGES.todaySchedule();
-    } else {
-      await PAGES.todaySchedule();
-    }
-  },
-};
 
 const main = async () => {
   await login();
