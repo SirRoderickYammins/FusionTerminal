c17ef26b69bb96ddbc618216d2130c85a9774412
Leke Dobruna <leke.dobruna@gmail.com>
2022-07-08 02:16:31 +0200

    Scaffolded a page system for UI

--- src/main.ts
+++ src/main.ts
@@ -5,26 +5,63 @@ import { login } from "./api";
 import { getTodaysSchedule, setSessionStatus } from "./matrix";
 import { initialActionSelect } from "./prompts/initialActionSelect";
 import { getTodaySelection } from "./prompts/getTodaySelect";
+import { getSessionActions } from "./prompts/getSessionActions";
+import { Session } from "./types/sessionTypes";
+
+const PAGES: Record<string, Function> = {
+  homepage: async () => {
+    const response = await initialActionSelect();
+    if (response.initialActionSelect === "lookSchedule") {
+      return { page: "todaySchedule", args: [] };
+    } else {
+      return undefined;
+    }
+  },
+  todaySchedule: async () => {
+    console.log("Loading schedule...");
+    const schedule = await getTodaysSchedule();
+    console.clear();
+    const response = await getTodaySelection(schedule.sessions);
+    if (response.sessionSelect === -1) {
+      return { page: "homepage", args: [] };
+    }
+    const session = schedule.sessions[response.sessionSelect];
+
+    return { page: "sessionStuff", args: [session] };
+  },
+  sessionStuff: async (session: Session) => {
+    const response = await getSessionActions(session);
+    if (response.sessionActions === "attended") {
+      // TODO: Set the status to true
+      // shit.sessions[0].appointmentStyle
+    } else if (response.sessionActions === "unattended") {
+      // TODO: Set the status to false
+      // shit.sessions[0].appointmentStyle
+    } else {
+      return { page: "todaySchedule", args: [] };
+    }
+    console.log("Action:", response.sessionActions, "for", session.title);
+    return undefined;
+  },
+};
 
 const main = async () => {
-  console.clear();
   await login();
-  const response = await initialActionSelect();
-  console.clear();
 
-  switch (response.initialActionSelect) {
-    case "lookSchedule":
-      const schedule = await getTodaysSchedule();
-      const response = await getTodaySelection(schedule.sessions);
-      console.log(schedule.sessions[response.sessionSelect]);
-      break;
+  // The default page should always be homepage
+  let next: { page: string; args: any[] } | undefined = {
+    page: "homepage",
+    args: [],
+  };
 
-    default:
-      console.log("get fucked");
-      break;
-  }
-
-  // console.log(shit.sessions[0].appointmentStyle);
+  do {
+    console.clear();
+    if (PAGES[next.page]) {
+      next = await PAGES[next.page](...next.args);
+    } else {
+      console.log(`Error: ${next.page} doesn't exist.`);
+    }
+  } while (next);
 };
 
 main().catch(console.error);
--- src/prompts/getTodaySelect.ts
+++ src/prompts/getTodaySelect.ts
@@ -7,13 +7,19 @@ export const getTodaySelection = async (sessions: Session[]) => {
       type: "select",
       name: "sessionSelect",
       message: "Select the Session You Wish to Edit",
-      choices: sessions.map((session, index) => {
-        return {
-          title: session.title,
-          description: `[${session.sessionNumber}/${session.sessionCountTotal}]`,
-          value: index,
-        };
-      }),
+      choices: [
+        ...sessions.map((session, index) => {
+          return {
+            title: session.title,
+            description: `[${session.sessionNumber}/${session.sessionCountTotal}]`,
+            value: index,
+          };
+        }),
+        {
+          title: "Go back",
+          value: -1,
+        },
+      ],
 
       initial: 0,
     },
