0c36206a938306edf51c99a8cde7fb67b86dcdb1
Leke <leke.dobruna@gmail.com>
2022-07-09 23:11:56 +0200

    Added logic to find free slots in the schedule

--- src/api.ts
+++ src/api.ts
@@ -3,8 +3,15 @@ import { wrapper } from "axios-cookiejar-support";
 import { CookieJar } from "tough-cookie";
 import { getCurrentUser, getUserHours } from "./matrix";
 import { UserPlanningTime, BookingInformation } from "./types/sessionTypes";
-import { format, startOfWeek, endOfWeek, addDays, addHours } from "date-fns";
-import { utcToZonedTime } from "date-fns-tz";
+import {
+  startOfWeek,
+  addDays,
+  setHours,
+  addMinutes,
+  setMinutes,
+  differenceInMinutes,
+} from "date-fns";
+import { utcToZonedTime, format } from "date-fns-tz";
 import { currentUser } from "./current-user";
 
 const jar = new CookieJar();
@@ -44,19 +51,102 @@ export const PlanningTimeBalance = async (): Promise<
   return planningTimeMins.earnedPlanningTime.planningTimeBalanceMinutes;
 };
 
-export const GetScheduleFreeTime = (booking_info: BookingInformation) => {
-  const bookedSlots = booking_info.reservations.map((eachClass) => {
-    return {
-      startDate: new Date(eachClass.startDate),
-      endDate: new Date(eachClass.endDate),
-    };
+const formatDate = (date: Date) => {
+  return format(date, "yyyy-MM-dd'T'HH:mm:ssXXXXX", {
+    timeZone: currentUser.iana,
   });
+};
 
-  console.log(bookedSlots);
-  const currentDate = utcToZonedTime(new Date(), currentUser.iana);
-  const mondayOfWorkWeek = format(
-    addHours(addDays(startOfWeek(currentDate), 1), 7),
-    "yyyy-MM-dd'T'HH:mm:ssXXXXX"
+export const GetScheduleFreeTime = (booking_info: BookingInformation) => {
+  const { reservations: bookedSlots } = booking_info;
+
+  // Get the date for the start of the week
+  // The reason we're getting it at 6:30 is because the while loop below adds 30 minutes in the beginning of it
+  let date = setMinutes(
+    setHours(
+      addDays(startOfWeek(utcToZonedTime(new Date(), currentUser.iana)), 1),
+      6
+    ),
+    30
   );
-  console.log(mondayOfWorkWeek);
+
+  // This is where all of the free slot times will be stored
+  let freeSlots: {
+    startDate: string;
+    endDate: string;
+  }[] = [];
+  // This is a temporary variable to store the current free slot while we loop through the week
+  let currentFreeSlot:
+    | { startDate: string | undefined; endDate: string | undefined }
+    | undefined;
+  // We want to keep an eye on the allocatedTime, and stop the loop when we've reached our total time
+  // the total time will probably have to be fed as a parameter
+  let allocatedTime = 0;
+  const totalTimeAvailable = 420;
+  // We want to know when is the end of a day time so we can immediately move to the next day
+  const endOfDayTime = "19:00:00";
+
+  while (allocatedTime < totalTimeAvailable) {
+    date = addMinutes(date, 30);
+
+    const isEndOfDay =
+      format(date, "HH:mm:ss", { timeZone: currentUser.iana }) === endOfDayTime;
+
+    if (isEndOfDay) {
+      if (currentFreeSlot) {
+        freeSlots.push({
+          startDate: currentFreeSlot.startDate as string,
+          endDate: formatDate(date),
+        });
+        allocatedTime += 30;
+        currentFreeSlot = undefined;
+      }
+
+      date = setMinutes(setHours(addDays(date, 1), 6), 30);
+
+      // TODO: Add a check to see if we're reached the weekend, if yes, kill the loop
+      continue;
+    }
+
+    const conflict = bookedSlots.find(
+      (slot) => slot.startDate === formatDate(date)
+    );
+
+    if (!conflict && !currentFreeSlot) {
+      currentFreeSlot = {
+        startDate: formatDate(date),
+        endDate: undefined,
+      };
+    } else if (!conflict && currentFreeSlot) {
+      currentFreeSlot.endDate = formatDate(date);
+      allocatedTime += 30;
+    } else if (conflict && currentFreeSlot) {
+      allocatedTime += 30;
+      freeSlots.push({
+        startDate: currentFreeSlot.startDate as string,
+        endDate: formatDate(date),
+      });
+      currentFreeSlot = undefined;
+
+      // We want to know how many minutes the booked session has so that we can move that amount of time forward
+      const minutesBetween = differenceInMinutes(
+        new Date(conflict.endDate),
+        new Date(conflict.startDate)
+      );
+      addMinutes(date, minutesBetween - 30);
+    }
+
+    if (allocatedTime >= totalTimeAvailable) {
+      if (currentFreeSlot) {
+        freeSlots.push({
+          startDate: currentFreeSlot.startDate as string,
+          endDate: formatDate(date),
+        });
+      }
+      break;
+    }
+  }
+
+  // console.log(allocatedTime, "/", totalTimeAvailable);
+  console.log("Free slots:", freeSlots);
 };
